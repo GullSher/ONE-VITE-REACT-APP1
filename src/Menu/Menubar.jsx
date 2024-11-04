70a852b79b5f5ebe6dd5c8311d6275276fc4c0db
GullSher <GullSher73@gamil.com>
2024-11-04 15:23:30 +0500

    aa

--- src/Menu/Menubar.jsx
+++ src/Menu/Menubar.jsx
@@ -10,12 +10,30 @@ function Navbar1() {
     // <Navbar expand="lg" className="bg-body-tertiary"> 
     <Navbar expand="lg" className="custom-navbar"> 
       <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">ROADWOODS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+        
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Products" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+
+
+            
+
+            <Nav.Link href="#link">SERVICES</Nav.Link>
+            <Nav.Link href="#link">CONTACT US</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
